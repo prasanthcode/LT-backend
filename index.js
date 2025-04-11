d6baf9850295126141035a7987a6637383885cec
Hi Prasanth...!!! <pintuprasanth9@gmail.com>
2025-04-11 23:34:37 +0530

    cors fix

--- index.js
+++ index.js
@@ -14,22 +14,23 @@ import countryRoutes from './routes/country.js';
 
 const PORT = process.env.PORT || 5000;
 const app = express();
-const allowedOrigins = ['http://localhost:3000', '*'];
-app.use(cors({
-  origin: 'http://localhost:3000', 
-  methods: 'GET,POST,PUT,DELETE', 
-  allowedHeaders: 'Content-Type,Authorization' 
-}));
+const allowedOrigins = ['*'];
+// app.use(cors({
+//   origin: 'http://localhost:3000', 
+//   methods: 'GET,POST,PUT,DELETE', 
+//   allowedHeaders: 'Content-Type,Authorization' 
+// }));
+// app.use(cors());
 
-app.use(cors({
-  origin: function (origin, callback) {
-    if (allowedOrigins.indexOf(origin) !== -1 || !origin) {
-      callback(null, true);
-    } else {
-      callback(new Error('Not allowed by CORS'));
-    }
-  }
-}));
+// app.use(cors({
+//   origin: function (origin, callback) {
+//     if (allowedOrigins.indexOf(origin) !== -1 || !origin) {
+//       callback(null, true);
+//     } else {
+//       callback(new Error('Not allowed by CORS'));
+//     }
+//   }
+// }));
 
 // Middleware
 app.use(cors()); 
